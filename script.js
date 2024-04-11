7adbdb186715a36478eefb0d2fb783befae03d8e
bobevans <caiwendi88@gmail.com>
2024-04-12 00:05:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -213,7 +213,13 @@ async function testIPs(ipList) {
     }
     testNo++;
     let testResult = 0;
-    let url = `${protocol}://${ip}:${portNo}/cdn-cgi/trace`;
+    let url = null;
+    if (protocol == 'https') {
+      url = `https://${ip}:${portNo}/__down`;
+    } else {
+      url = `http://${ip}:${portNo}/cdn-cgi/trace`;
+    }
+    // let url = `${protocol}://${ip}:${portNo}/cdn-cgi/trace`;
 
     const startTime = performance.now();
     const controller = new AbortController();
